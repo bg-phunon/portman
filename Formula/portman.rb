85259620aa7f82c475668bf81e7dd73d2a06358a
bg-phunon <bg.phunon@gmail.com>
2026-04-22 23:08:35 +0700

    feat: update Homebrew formula for v0.2.2

--- Formula/portman.rb
+++ Formula/portman.rb
@@ -1,8 +1,8 @@
 class Portman < Formula
   desc "TUI tool for monitoring and managing processes listening on TCP ports"
   homepage "https://github.com/bg-phunon/portman"
-  url "https://github.com/bg-phunon/portman/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0c4b21d8fa7dace126edb883e70107efb88bb0dec620e1ad3bd7f8b4546be48b"
+  url "https://github.com/bg-phunon/portman/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "636dedd9b90b3fd3012e77efe99704f2c2cf82b213a2d33533f6f53acc5b13bc"
   license "MIT"
   head "https://github.com/bg-phunon/portman.git", branch: "main"
 
